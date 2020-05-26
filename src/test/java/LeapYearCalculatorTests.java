bd86d66d442cbe8703d7133466359429ff1d27cb
Akshay Vadher <avadher@fpg-ingauge.com>
2020-05-26 16:16:34 +0530

    the test case for 2nd step. Even if it fails. Interestingly it passes without any change in logic

--- src/test/java/LeapYearCalculatorTests.java
+++ src/test/java/LeapYearCalculatorTests.java
@@ -13,4 +13,11 @@ public class LeapYearCalculatorTests {
         LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
         assertTrue(leapYearCalculator.isLeapYear(2000));
     }
+
+    @DisplayName("All years divisible by 100 but not by 400 are NOT leap years (so, for example, 1700, 1800, and 1900 were NOT leap years, NOR will 2100 be a leap year)")
+    @Test
+    public void test_that_years_divisible_by_100_but_not_400_are_not_leap_years(){
+        LeapYearCalculator leapYearCalculator = new LeapYearCalculator();
+        assertFalse(leapYearCalculator.isLeapYear(1700));
+    }
 }
